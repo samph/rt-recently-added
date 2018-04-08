0abde56fbd99d2130310342f37e0a3f893e78d97
Sam Phelps <drakethes@gmail.com>
2018-04-08 10:01:58 +0100

    Add dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,4 @@
 FROM python:3.6-alpine
 COPY requirements.txt /requirements.txt
 RUN pip3 install -r /requirements.txt
-COPY app /app
 COPY rt-ra.py /rt-ra.py
-WORKDIR /app
--- run_docker.sh
+++ run_docker.sh
@@ -1,1 +1,1 @@
-docker run -d --name rt-ra -p 5000:5000 -e FLASK_APP=/rt-ra.py rt-ra flask run --host=0.0.0.0
+docker run -v $(pwd)/app:/app -t -d --name rt-ra -p 5000:5000 -e FLASK_DEBUG=1 -e FLASK_APP=/rt-ra.py rt-ra flask run --host=0.0.0.0
